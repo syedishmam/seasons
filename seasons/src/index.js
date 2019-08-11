95947434b9f31df6e16939e9fb41f8b954ec1d16
Syed Ishmam <syedishmam14@gmail.com>
2019-08-11 16:01:14 -0400

    Convert App to class component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
-
-    return (
-        <div>Hi there!</div>
-    );
+class App extends React.Component {  
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err)
+        );
+        
+        return (
+            <div>Latitude: </div>
+        );
+    }
 }
 
 ReactDOM.render(
