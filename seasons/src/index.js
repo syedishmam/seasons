f8dc5d3f79151ad7e9012ae428bfe7b8b37341d8
Syed Ishmam <syedishmam14@gmail.com>
2019-08-11 14:55:55 -0400

    First commit

---
+++ seasons/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+
+const App = () => {
+    return (
+        <div>Hi there!</div>
+    );
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
