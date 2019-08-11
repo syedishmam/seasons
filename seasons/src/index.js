48298c48c51abf4575ce46f3f010ef73dfcfae94
Syed Ishmam <syedishmam14@gmail.com>
2019-08-11 16:12:44 -0400

    Add state to App

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
+//import SeasonDisplay from './SeasonDisplay';
 
-class App extends React.Component {  
+class App extends React.Component { 
+    constructor(props) {
+        super(props);
+        this.state= {
+            lat: null
+        };
+    }    
+    
     render() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => console.log(position),
             (err) => console.log(err)
         );
-        
+
         return (
             <div>Latitude: </div>
         );
