c5a1f13730a3ec8178ee3f76cd462356eb63c0d7
Syed Ishmam <syedishmam14@gmail.com>
2019-08-11 18:45:10 -0400

    Add seasonConfig() function

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -8,16 +8,26 @@ const getSeason = (lat, month) => {
     }
 }
 
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the beach",
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Burr, it is chilly',
+        iconName: 'snowflake'
+    }
+};
+
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, props.season);
-    const text = season === 'winter' ? 'Burr, it is chilly' : "Let's hit the beach";
-    const icon = season === 'winter' ? 'snowflake' : 'sun';
+    const {text, iconName} = seasonConfig[season]
 
     return (
         <div>
-            <i className={`${icon} icon`} />
+            <i className={`massive ${iconName} icon`} />
             <h1>{text}</h1>
-            <i className={`${icon} icon`} />
+            <i className={`massive ${iconName} icon`} />
         </div>
     );
 }
