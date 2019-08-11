ad12484842bab5d53695afcf0531806211a5b5d7
Syed Ishmam <syedishmam14@gmail.com>
2019-08-11 16:59:57 -0400

    Move getCurrentPosition to componentDidMount()

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -9,16 +9,14 @@ class App extends React.Component {
             lat: null,
             errorMessage: ''
         };
-
+    } 
+    
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                 this.setState({lat: position.coords.latitude});
-            },
-            (err) => {
-                this.setState({errorMessage: err.message});
-            }
+            position => this.setState({lat: position.coords.latitude}),
+            err => this.setState({errorMessage: err.message})
         );
-    }    
+    }
     
     render() {
         if(this.state.errorMessage && !this.state.lat) {
@@ -28,7 +26,7 @@ class App extends React.Component {
         if(!this.state.errorMessage && this.state.lat) {
             return <div>Latitude: {this.state.lat}</div>;
         }
-        
+
         return <div>Loading!</div>;
     }
 }
